13c31e5bb0332bcef9684be844db98da8ecd9c97
danaryu <dana40426@gmail.com>
2023-02-11 15:56:42 +0900

    feat: first push

---
+++ src/main/kotlin/world/haebang/talchul/TalchulApplication.kt
@@ -0,0 +1,11 @@
+package world.haebang.talchul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TalchulApplication
+
+fun main(args: Array<String>) {
+	runApplication<TalchulApplication>(*args)
+}
---
+++ src/test/kotlin/world/haebang/talchul/TalchulApplicationTests.kt
@@ -0,0 +1,13 @@
+package world.haebang.talchul
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TalchulApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
